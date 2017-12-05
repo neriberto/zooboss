8cbdb1f357d04063afd9007ca47a952b1359e021
Neriberto C.Prado <neriberto.prado@bluepex.com>
2017-12-05 15:57:59 -0200

    Document functions and renaming variables

--- zooboss.py
+++ zooboss.py
@@ -16,16 +16,18 @@ USE_MAGIC = False
 
 
 def get_file_type(file_path):
+    """Get file real file type."""
     try:
         import subprocess
         file_process = subprocess.Popen(
-                ['file', '-b', file_path], stdout=subprocess.PIPE)
+            ['file', '-b', file_path], stdout=subprocess.PIPE)
         return file_process.stdout.read().strip()
     except:
         return None
 
 
 def create_new_path(file_hash, file_type):
+    """Create a new file path."""
     if USE_MAGIC:
         if file_type is not None:
             mime = file_type.split()[0].lower()
@@ -36,25 +38,26 @@ def create_new_path(file_hash, file_type):
         directory = os.path.join(DESTINY_PATH)
 
     directory = os.path.join(
-            directory,
-            file_hash[0],
-            file_hash[1],
-            file_hash[2],
-            file_hash[3])
+        directory,
+        file_hash[0],
+        file_hash[1],
+        file_hash[2],
+        file_hash[3])
     if not os.path.isdir(directory):
         os.makedirs(directory)
     return os.path.join(directory, file_hash)
 
 
 def execute(file_path):
+    """Execute the file analysis."""
     if not os.path.isfile(file_path):
         return
-    with open(file_path, 'rb') as fd:
+    with open(file_path, 'rb') as file_descriptor:
         if USE_MAGIC:
             file_type = get_file_type(file_path)
         else:
             file_type = None
-        file_hash = hashlib.sha256(fd.read()).hexdigest()
+        file_hash = hashlib.sha256(file_descriptor.read()).hexdigest()
         logging.info(file_hash + ' ' + file_path)
         new_file_path = create_new_path(file_hash, file_type)
         # if path not exists
@@ -69,6 +72,7 @@ def execute(file_path):
 
 
 def worker():
+    """The function worker that runs in a thread."""
     global END_PROCESS
     global WORK
     while not END_PROCESS:
@@ -81,6 +85,7 @@ def worker():
 
 
 def threads_stop(threads):
+    """Stop the threads."""
     global END_PROCESS
     END_PROCESS = True
     for thread in threads:
@@ -89,15 +94,19 @@ def threads_stop(threads):
 
 
 def main(dir_path, n_threads):
+    """Start the threads."""
     global END_PROCESS
     global DIR_LISTED
 
-    logging.basicConfig(level=logging.INFO,
-            format='%(asctime)s %(levelname)-8s %(message)s',
-            datefmt='%y-%m-%d %H:%M:%S',
-            stream=sys.stdout)
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s %(levelname)-8s %(message)s',
+        datefmt='%y-%m-%d %H:%M:%S',
+        stream=sys.stdout)
     logging.getLogger("zooboss")
-    logging.addLevelName(logging.WARNING, '\033[1;31m%s\033[1;0m' % logging.getLevelName(logging.WARNING))
+    logging.addLevelName(
+        logging.WARNING,
+        '\033[1;31m%s\033[1;0m' % logging.getLevelName(logging.WARNING))
 
     try:
         threads = []
@@ -125,39 +134,39 @@ def main(dir_path, n_threads):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument(
-            "-o",
-            "--origin",
-            help='The path to be scanned',
-            action='store',
-            required=True)
+        "-o",
+        "--origin",
+        help='The path to be scanned',
+        action='store',
+        required=True)
     parser.add_argument(
-            "-d",
-            "--destiny",
-            help="The destiny path to store files",
-            action="store",
-            default=DESTINY_PATH,
-            required=False)
+        "-d",
+        "--destiny",
+        help="The destiny path to store files",
+        action="store",
+        default=DESTINY_PATH,
+        required=False)
     parser.add_argument(
-            "-m",
-            "--move",
-            help="Determines that the origin file must be moved",
-            action="store_true",
-            default=False,
-            required=False)
+        "-m",
+        "--move",
+        help="Determines that the origin file must be moved",
+        action="store_true",
+        default=False,
+        required=False)
     parser.add_argument(
-            "-f",
-            "--filetype",
-            help="Determines that the destiny will use the file type",
-            action="store_true",
-            default=False,
-            required=False)
+        "-f",
+        "--filetype",
+        help="Determines that the destiny will use the file type",
+        action="store_true",
+        default=False,
+        required=False)
     parser.add_argument(
-            "-t",
-            "--threads",
-            help="The number of threads",
-            action="store",
-            default=2,
-            required=False)
+        "-t",
+        "--threads",
+        help="The number of threads",
+        action="store",
+        default=2,
+        required=False)
 
     args = parser.parse_args()
 
